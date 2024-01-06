734559ccc87d0c7d9bda1d7abe52ace5a467f9a7
Chircea <dam.david.chircea@gmail.com>
2024-01-06 23:25:13 +0100

    base game

--- lib/bodies/TierraBody.dart
+++ lib/bodies/TierraBody.dart
@@ -5,6 +5,7 @@ import 'package:flame_forge2d/body_component.dart';
 import 'package:flame_forge2d/flame_forge2d.dart';
 import 'package:flame_tiled/flame_tiled.dart';
 
+//a
 class TierraBody extends BodyComponent{
 
   TiledObject tiledBody;
